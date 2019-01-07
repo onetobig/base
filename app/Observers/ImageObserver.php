87ebb15696e96738b784cea7e258fa3c19a9e3ed
onetobig <onetobig@gmail.com>
2019-01-07 10:50:54 +0800

    幻灯片上传压缩

--- app/Observers/ImageObserver.php
+++ app/Observers/ImageObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\Image;
 use Illuminate\Support\Facades\Storage;
 
@@ -10,9 +11,16 @@ class ImageObserver
     public function deleted(Image $image)
     {
         // 删除图片
-        $driver = config('filesystems.admin');
-        if (Storage::disk($driver)->exists($image->url) && !Image::query()->where('url', $image->url)->exists()) {
-            Storage::disk($driver)->delete($image->url);
+        if (Storage::disk('admin')->exists($image->url) && !Image::query()->where('url', $image->url)->exists()) {
+            Storage::disk('admin')->delete($image->url);
+        }
+    }
+
+    public function saved(Image $image)
+    {
+        if (Storage::disk('admin')->exists($image->url)) {
+            $path = Storage::disk('admin')->path($image->url);
+            app(ImageUploadHandler::class)->reduceSize($path, 676);
         }
     }
 }
